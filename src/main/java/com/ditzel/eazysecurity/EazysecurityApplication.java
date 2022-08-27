2cefe6bb5e8b889d1fe541e9536ee63cad5e016e
lditzel94 <lucianoditzel94@gmail.com>
2022-08-27 15:11:24 -0300

    Initial commit

---
+++ src/main/java/com/ditzel/eazysecurity/EazysecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ditzel.eazysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EazysecurityApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( EazysecurityApplication.class, args );
+    }
+
+}
---
+++ src/test/java/com/ditzel/eazysecurity/EazysecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ditzel.eazysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazysecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
